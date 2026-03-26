916bca71b869b4cb37a9df376f65667c91a80285
Lukas <lukas.soderlund.fanta@gmail.com>
2026-03-26 10:34:28 +0100

    Change typeDef schema

--- schema/typeDefs.js
+++ schema/typeDefs.js
@@ -1,15 +1,99 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require('apollo-server-express')
 
 const typeDefs = gql`
-  type Book {
+  type Product {
     id: ID!
-    title: String!
-    publicationYear: Int
+    articleId: Int
+    varNumber: Int
+    name: String!
+    name2: String
+    price: Float
+    volumeMl: Float
+    pricePerLiter: Float
+    salesStart: String
+    discontinued: Boolean
+    productGroup: String
+    type: String
+    style: String
+    packaging: String
+    sealType: String
+    origin: String
+    originCountry: String
+    producer: String
+    supplier: String
+    vintage: Int
+    alcoholContent: String
+    assortmentText: String
+    organic: Boolean
+    ethical: Boolean
+    kosher: Boolean
+    rawMaterials: String
+  }
+
+  type Country {
+    name: String!
+    productCount: Int!
+    products: [Product!]!
+  }
+
+  type ProductGroup {
+    name: String!
+    productCount: Int!
+    products: [Product!]!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+  }
+
+  type AuthPayload {
+    token: String!
+    user: User!
+  }
+
+  type ProductsResult {
+    products: [Product!]!
+    total: Int!
+    page: Int!
+    pages: Int!
   }
 
   type Query {
-    books: [Book]
+    products(page: Int, limit: Int, search: String, productGroup: String, country: String): ProductsResult!
+    product(id: ID!): Product
+    countries: [Country!]!
+    productGroups: [ProductGroup!]!
+    me: User
   }
-`;
 
-module.exports = typeDefs;
+  type Mutation {
+    register(username: String!, email: String!, password: String!): AuthPayload!
+    login(email: String!, password: String!): AuthPayload!
+    addProduct(
+      name: String!
+      price: Float
+      productGroup: String
+      type: String
+      originCountry: String
+      alcoholContent: String
+      volumeMl: Float
+      producer: String
+    ): Product!
+    updateProduct(
+      id: ID!
+      name: String
+      price: Float
+      productGroup: String
+      type: String
+      originCountry: String
+      alcoholContent: String
+      volumeMl: Float
+      producer: String
+    ): Product!
+    deleteProduct(id: ID!): Boolean!
+  }
+`
+
+module.exports = typeDefs
